d23b11d9788f20ad8b992ef7f3617c43b921718f
Sam De Boni <sdeboni@gmail.com>
2024-10-22 11:04:37 -0400

    handle simple header

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,12 @@ struct Header {
 }
 
 #[allow(dead_code)]
-fn parse_header(_line: &str) -> Header {
+fn parse_header(line: &str) -> Header {
+    let field_delimiter_index = line.find(":").unwrap();
+
     Header {
-        field: "".to_string(),
-        body: "".to_string(),
+        field: line[0..field_delimiter_index].to_string(),
+        body: line[field_delimiter_index + 1..].to_string(),
     }
 }
 
@@ -23,7 +25,7 @@ mod tests {
             field: "this".to_string(),
             body: "is a simple header".to_string(),
         };
-        let actual_header = parse_header("this: is a simple header");
+        let actual_header = parse_header("this:is a simple header");
         assert_eq!(expected_header, actual_header);
     }
 }
